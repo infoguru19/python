a3e691f92faf1ff75b24520c8c0b248e5d6373dd
Ravi Ranjan <51418554+infoguru19@users.noreply.github.com>
2026-04-02 14:10:07 +0530

    Create dictionary.py

---
+++ 8. Collection of datatypes/dictionary.py
@@ -0,0 +1,15 @@
+# Dictionaries are used to store data values in key:value pairs.
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(thisdict)
+
+print(thisdict.get("model"))
+
+x = thisdict["year"]
+print(x)
