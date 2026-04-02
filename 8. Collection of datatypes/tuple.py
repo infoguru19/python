c35748cc568229a45f651f7ec16b8c7036b8585a
Ravi Ranjan <51418554+infoguru19@users.noreply.github.com>
2026-04-02 13:56:17 +0530

    Create tuple.py

---
+++ 8. Collection of datatypes/tuple.py
@@ -0,0 +1,6 @@
+# A tuple is a collection which is ordered and unchangeable.
+# When we say that tuples are ordered, it means that the items have a defined order, and that order will not change.
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+print(len(thistuple))
