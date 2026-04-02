955aa0cb4c760883e42ffcb81b8f103e0e04beea
Ravi Ranjan <51418554+infoguru19@users.noreply.github.com>
2026-04-02 10:39:47 +0530

    Create challenge.py

---
+++ 5. if_else_case/challenge.py
@@ -0,0 +1,18 @@
+# Challenge
+"""
+1. Create a variable age with the value 20
+2. Write an if statement that prints "Child" if age is less than 13
+3. Add an elif that prints "Teenager" if age is less than 18
+4. Add an else that prints "Adult
+""""
+#Solution
+
+# Create age variable
+age = 20
+# Write if/elif/else
+if age < 13:
+  print("Child")
+elif age < 18:
+  print("Teenager")
+else:
+  print("Adult")
