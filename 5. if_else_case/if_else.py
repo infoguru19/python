6391910ac88f233fa779f565e124efe8f0e92a85
Ravi Ranjan <51418554+infoguru19@users.noreply.github.com>
2026-04-02 10:39:28 +0530

    Create if_else.py

---
+++ 5. if_else_case/if_else.py
@@ -0,0 +1,37 @@
+a = 33
+b = 200
+if b > a:
+  print(f"{b} is greater than {a}")
+  print("b is greater")
+  
+# Checking if number is positive or not
+a = 10
+if a > 0:
+    print(f"{a} is positive number")
+    
+# Using variable in condition
+log_in = False
+if log_in:
+    print("Welcome Back !")
+   
+ 
+#else if
+a = 300
+b = 200
+if b > a:
+  print(f"{b} is greater than {a}")
+elif a > b:
+    print(f"{a} is greater than {b}")
+elif a == b:
+    print(f"{a} is equal to {b}")
+
+# Else
+a = 300
+b = 200
+if b > a:
+  print(f"{b} is greater than {a}")
+elif a == b:
+    print(f"{b} is equal to {a}")
+else: 
+    print(f"{b} is less than {a}")
+    
