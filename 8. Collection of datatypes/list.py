2e1c2e76c1cdb70156cb5dc923bb5780257a346f
Ravi Ranjan <51418554+infoguru19@users.noreply.github.com>
2026-04-02 13:53:27 +0530

    Update list.py

--- 8. Collection of datatypes/list.py
+++ 8. Collection of datatypes/list.py
@@ -1,2 +1,9 @@
-mylist = ["apple", "banana", "cherry"]
+# Lists are used to store multiple items in a single variable.
+
+mylist = ["apple", "grapes", "banana", "banana", "cherry"]
 print(mylist)
+print(mylist[3] + " "+mylist[0])
+
+mylist.sort()
+print(mylist)
+
