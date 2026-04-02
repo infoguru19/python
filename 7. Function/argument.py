b0f4ddd8c95e2b26c95e630eb44e302f13b03763
Ravi Ranjan <51418554+infoguru19@users.noreply.github.com>
2026-04-02 13:42:20 +0530

    Update argument.py

--- 7. Function/argument.py
+++ 7. Function/argument.py
@@ -1,3 +1,9 @@
 def name(fname, lname):
   print(fname + " "+lname)
 name("ravi", "ranjan")
+
+# Defult Argument
+def name(fname = "ravi", lname = "raj"):
+  print(fname + " "+lname)
+name()
+name("John", "Rocky")
